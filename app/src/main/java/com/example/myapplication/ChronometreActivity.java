f0e5d4aa5970caa5e822da71cd97cfc06dd4c1c6
demang179u <DA77de37&*>
2022-09-20 15:47:51 +0200

    chrono début

--- app/src/main/java/com/example/myapplication/ChronometreActivity.java
+++ app/src/main/java/com/example/myapplication/ChronometreActivity.java
@@ -1,5 +1,9 @@
 package com.example.myapplication;
 
+import android.util.Log;
+import android.widget.Button;
+import android.widget.ImageButton;
+import android.widget.Toast;
 import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
 
@@ -7,7 +11,13 @@ public class ChronometreActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+        Log.i("ChronometreActivity", "onCreate: ");
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_chronometre);
+        Toast.makeText(ChronometreActivity.this,"Chronomètre", Toast.LENGTH_SHORT).show();
+
+        ImageButton start = findViewById(R.id.start);
+        ImageButton stop = findViewById(R.id.stop);
+
     }
 }
--- app/src/main/java/com/example/myapplication/MainActivity.java
+++ app/src/main/java/com/example/myapplication/MainActivity.java
@@ -1,6 +1,9 @@
 package com.example.myapplication;
 
+import android.content.Intent;
+import android.util.Log;
 import android.widget.Button;
+import android.widget.Toast;
 import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
 
@@ -8,9 +11,25 @@ public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+        Log.i("MainActivity", "onCreate: ");
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        Button bonjour = 
+        Button bonjour = findViewById(R.id.bonjour);
+        Button chrono = findViewById(R.id.chrono);
+        Button photo = findViewById(R.id.photo);
+
+        bonjour.setOnClickListener(view -> Toast.makeText(MainActivity.this,"Bonjour", Toast.LENGTH_SHORT).show());
+        chrono.setOnClickListener(view -> startActivities(new Intent[]{new Intent(MainActivity.this, ChronometreActivity.class)}));
+
+
+
+
+
+
+
+
+
+
     }
 }
